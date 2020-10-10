bd6180f1cbbd6fac02e44beed1ca01f30fd46813
charaph <72637175+charaph@users.noreply.github.com>
2020-10-10 13:39:27 +0700

    test sync

---
+++ CHADEV/XtraForm7.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace CHADEV
+{
+    partial class XtraForm7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xtraUserControl21 = new CHADEV.XtraUserControl2();
+            this.SuspendLayout();
+            // 
+            // xtraUserControl21
+            // 
+            this.xtraUserControl21.Location = new System.Drawing.Point(-15, 34);
+            this.xtraUserControl21.Name = "xtraUserControl21";
+            this.xtraUserControl21.Size = new System.Drawing.Size(415, 299);
+            this.xtraUserControl21.TabIndex = 0;
+            // 
+            // XtraForm7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 268);
+            this.Controls.Add(this.xtraUserControl21);
+            this.Name = "XtraForm7";
+            this.Text = "XtraForm7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private XtraUserControl2 xtraUserControl21;
+    }
+}
